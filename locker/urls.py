4181c957574f93f3ff627d2f4295100081bc927f
Kazim Muhammad <kazmodeston1@gmail.com>
2021-05-18 20:55:38 +0100

    completed apiOverview

--- locker/urls.py
+++ locker/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.apiOverview)
+    path('', views.apiOverview, name="apiOverview")
 ]
--- locker/views.py
+++ locker/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
 
+@api_view(['GET'])
 def apiOverview(request):
     api_urls = {
         'List' : "/locker-list/",
         'Create' : "/locker-create/",
         'Detail View' : "/locker-detail/<int:id>",
-        'Search' : "/locker-search/<str:city>",
+        'Search' : "/locker-search/<str:city>"
     }
 
     return Response(api_urls)
