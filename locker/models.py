0c891467d4a7815785204b2e0f365376d53b3595
Kazim Muhammad <kazmodeston1@gmail.com>
2021-05-18 20:40:56 +0100

    migrating models and creating apiOverview

--- locker/models.py
+++ locker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Locker(models.Model):
+    city = models.CharField(max_length = 200, null=False, blank=False)
+    state = models.CharField(max_length = 200, null=False, blank=False)
+    locker_name = models.CharField(max_length = 200, null=False, blank=False)
+    price_description = models.TextField()
+    locker_description = models.TextField()
+    date    = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length = 100, null=False, blank=False)
+
+    def __str__(self):
+        return self.state
--- locker/views.py
+++ locker/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
 # Create your views here.
+
+def apiOverview(request):
+    api_urls = {
+        'List' : "/locker-list/",
+        'Create' : "/locker-create/",
+        'Detail View' : "/locker-detail/<int:id>",
+        'Search' : "/locker-search/<str:city>",
+    }
+
+    return Response(api_urls)
